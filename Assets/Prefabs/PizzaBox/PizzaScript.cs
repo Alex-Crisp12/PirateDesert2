ccf0ea9cda4a173b5576a686f0c1a8f2caaf10c7
Alex Crisp <Alexander2.Crisp@live.uwe.ac.uk>
2024-03-01 14:00:22 +0000

    Pizza Box :)

---
+++ Assets/Prefabs/PizzaBox/PizzaScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaScript : MonoBehaviour
+{
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exiting collider");
+        if (other.tag == "Player")
+        {
+            animator.SetTrigger("Close");
+        }
+    }
+}
