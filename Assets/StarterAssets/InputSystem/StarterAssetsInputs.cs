095ad82bc516410b460f8abe262cffba5b39693c
Alex Crisp <Alexander2.Crisp@live.uwe.ac.uk>
2024-04-23 15:33:53 +0100

    ready to build

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,7 +12,6 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
-		public bool crouch;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -44,11 +43,6 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
-
-		public void OnCrouch(InputValue value)
-		{
-			CrouchInput(value.isPressed);
-		}
 #endif
 
 
@@ -70,11 +64,6 @@ namespace StarterAssets
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
-		}
-
-		public void CrouchInput(bool newCrouchState)
-		{
-			crouch = newCrouchState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
